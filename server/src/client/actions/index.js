cc67c3a1eda66e6764b030146f14f9db62bd74c8
kshukost <christina.shukost@gmail.com>
2021-08-16 01:10:35 +0300

    Client and Server Axios Instances

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,10 +1,7 @@
-import axios from 'axios';
-
-const ROOT_URL = 'https://react-ssr-api.herokuapp.com';
-
 export const FETCH_USERS = 'FETCH_USERS';
-export const fetchUsers = () => async dispatch => {
-  const res = await axios.get(`${ROOT_URL}/users`);
+
+export const fetchUsers = () => async (dispatch, getState, api) => {
+  const res = await api.get('/users');
 
   dispatch({
     type: FETCH_USERS,
