2799bb76bc868183175c43cbda5b9ed26ce8a56c
kshukost <christina.shukost@gmail.com>
2021-08-21 21:50:27 +0300

    Admins Action Creator and Reducer

--- server/src/client/reducers/index.js
+++ server/src/client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './usersReducer';
 import authReducer from './authReducer';
+import adminsReducers from './adminsReducers';
 
 export default combineReducers({
   users: usersReducer,
   auth: authReducer,
+  admins: adminsReducers,
 });
