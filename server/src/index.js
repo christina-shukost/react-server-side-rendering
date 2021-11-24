922a914f5164d00d4d1aedae04c7a801721020e4
kshukost <christina.shukost@gmail.com>
2021-11-24 20:21:50 +0200

    implemented requireAuth HOC

--- server/src/index.js
+++ server/src/index.js
@@ -23,19 +23,30 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore(req);
 
-  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+  const promises = matchRoutes(Routes, req.path)
+    .map(({ route }) => {
+      return route.loadData ? route.loadData(store) : null;
+    })
+    .map(promise => {
+      if (promise) {
+        return new Promise((resolve, reject) => {
+          promise.then(resolve).catch(resolve);
+        });
+      }
+    });
 
-  Promise.all(promises).then(() => {
-    const context = {};
-    const content = renderer(req, store, context);
+  Promise.all(promises)
+    .then(() => {
+      const context = {};
+      const content = renderer(req, store, context);
 
-    if(context.notFound) {
-      res.status(404);
-    }
+      if(context.notFound) {
+        res.status(404);
+      }
 
-    res.send(content);
+      res.send(content);
+    }).catch(() => {
+      res.send('Something went wrong');
   });
 });
 
